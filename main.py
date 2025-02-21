4c4ba1977139f02c2627dc2abb2a535c4f721fae
kmironov09@gmail.com <kmironov09@gmail.com>
2025-02-21 23:30:01 +0300

    konstantin

--- main.py
+++ main.py
@@ -1,86 +1,60 @@
-from operator import index
 import sqlite3
 import telebot
 from telebot import types
-global list
-global inp
-inp = "0"
-connection = sqlite3.connect('my_database.db', check_same_thread=False)
+global inp#variable for input
+connection = sqlite3.connect('my_database.db', check_same_thread=False)#connect
 cursor = connection.cursor()
-plan = []
-token='7896437955:AAF6meLEqWoFPg6dLm2wKtpkidCcBXKSKas'
-bot=telebot.TeleBot(token)
+token='7896437955:AAF6meLEqWoFPg6dLm2wKtpkidCcBXKSKas'#token tg
+bot=telebot.TeleBot(token)#for telebot
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS Users (
 username INTEGER PRIMARY KEY,
 plan TEXT NOT NULL,
 score INTEGER
 )
-''')
-@bot.message_handler(commands=['start'])
+''')#make db
+@bot.message_handler(commands=['start'])#faund start massage
 def start_message(message):
-
     print(message.chat.id)
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("план")
-    btn2 = types.KeyboardButton("счет")
-    btn3 = types.KeyboardButton("ввод план")
-    markup.add(btn1, btn2, btn3)
-    cursor.execute('INSERT INTO Users (username, plan, score) VALUES (?, ?, ?)', (int(message.chat.id), " ", 0))
-    connection.commit()
-    bot.send_message(message.chat.id, "здесь будет инструкция ", reply_markup = markup)
-#cursor.execute('INSERT INTO Users (username) VALUES (?)', (str(message.chat.id)))
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)#for button
+    btn1 = types.KeyboardButton("план")# button plan
+    btn2 = types.KeyboardButton("счет")# button score
+    btn3 = types.KeyboardButton("ввод план")# input plan
+    markup.add(btn1, btn2, btn3)#show plan
+    cursor.execute('INSERT INTO Users (username, plan, score) VALUES (?, ?, ?)', (int(message.chat.id), " ", 0))# start in db
+    connection.commit()#save db
+    bot.send_message(message.chat.id, "здесь будет инструкция ", reply_markup = markup)#start message
 
 @bot.message_handler(content_types=['text'])
 def func(message):
-    global inp
-    if(message.text == "план"):
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("счет")
-        btn3 = types.KeyboardButton("ввод план")
-        back = types.KeyboardButton("Вернуться в главное меню")
-        cursor.execute("SELECT  plan FROM users WHERE username = ?", (message.from_user.id,))
-        plan = str(cursor.fetchall())
-        plan = plan[3:-4]
-        bot.send_message(message.chat.id, text="вот ваш план" + ": " + plan, reply_markup = markup)
-        for i in plan.split(";"):
-            markup = types.InlineKeyboardMarkup()
-            button1 = types.InlineKeyboardButton(i, callback_data=i)
-            markup.add(button1)
-            bot.send_message(message.chat.id, i.format(message.from_user), reply_markup=markup)
-        markup.add(btn1, btn3, back)
-        inp = "0"
-    elif(message.text == "счет"):
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("план")
-        btn3 = types.KeyboardButton("ввод план")
-        back = types.KeyboardButton("Вернуться в главное меню")
-        cursor.execute("SELECT  score FROM users WHERE username = ?", (message.from_user.id,))
-        score = str(cursor.fetchall())
+    global inp#variable for input
+    if message.text == "план":#plan
+        cursor.execute("SELECT  plan FROM users WHERE username = ?", (message.from_user.id,))#take plan in db
+        plan = str(cursor.fetchall())[3:-4]#take plan in db and delete extra
+        bot.send_message(message.chat.id, text="вот ваш план" + ": " + plan)#output plan
+        for i in plan.split(";"):#cycle for do inline but
+            markup = types.InlineKeyboardMarkup()#for inline
+            button1 = types.InlineKeyboardButton(i, callback_data=i)#output inline
+            markup.add(button1)#show inline
+            bot.send_message(message.chat.id, i.format(message.from_user), reply_markup = markup)#send text
+        inp = "0"#variable for input
+    elif message.text == "счет":#score
+        cursor.execute("SELECT  plan FROM users WHERE username = ?", (message.from_user.id,))#take score in db
+
+        score = str(cursor.fetchall()).count("✅")
         print(score)
-        bot.send_message(message.chat.id, text="вот ваш счет" + ": " + score[2:-3], reply_markup = markup)
-        markup.add(btn1, btn3, back)
-        inp = "0"
-    elif (message.text == "ввод план"):
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("план")
-        btn2 = types.KeyboardButton("счет")
-        btn3 = types.KeyboardButton("ввод план")
-        markup.add(btn1, btn2, btn3)
-        inp = message.from_user.id
-        bot.send_message(message.chat.id, text="жду", reply_markup = markup)
-    elif inp == message.from_user.id:
-        planin = str(message.text)
-        print(planin+"1")
-        cursor.execute('UPDATE Users SET plan = ? WHERE username = ?', (planin, message.from_user.id))
-        connection.commit()
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("план")
-        btn2 = types.KeyboardButton("счет")
-        btn3 = types.KeyboardButton("ввод план")
-        inp = "0"
-        markup.add(btn1, btn2, btn3)
-        bot.send_message(message.chat.id, text="план введен!", reply_markup=markup)
+        bot.send_message(message.chat.id, text="вот ваш счет" + ": " + str(score))#output score
+        inp = "0"#variable for input
+    elif message.text == "ввод план":# input start plan
+        inp = message.from_user.id#variable for input
+        bot.send_message(message.chat.id, text="жду")#output info
+    elif inp == message.from_user.id: # input plan
+        planin = str(message.text)# message in "str"
+        print(planin+"52")
+        cursor.execute('UPDATE Users SET plan = ? WHERE username = ?', (planin, message.from_user.id))#save in db
+        connection.commit()# save change
+        inp = "0"#variable for input
+        bot.send_message(message.chat.id, text="план введен!")#output info
 
 
 
@@ -90,11 +64,11 @@ def callback_query(call):
     cursor.execute("SELECT  plan FROM users WHERE username = ?", (call.from_user.id, ))
     plan = str(cursor.fetchall())
     plan = plan[3:-4]
-    print(call.data)
-    print(plan)
-
+    print(call.data+"66")
+    print(plan+"67")
     index = plan.find(str(call.data))
-    if index != -1:
+    print(plan[index]+"69")
+    if index != -1 and plan[index] != "✅" :
         bot.answer_callback_query(call.id, call.data )
         string_list = plan
         string_list = string_list[:index] +"✅"+ string_list[index:]
@@ -103,11 +77,13 @@ def callback_query(call):
         cursor.execute('UPDATE Users SET plan = ? WHERE username = ?', (plan, call.from_user.id))
         connection.commit()
     else:
-        bot.answer_callback_query(call.id, "ds" )
+        bot.answer_callback_query(call.id, "задача выполнена" )
 
         
       
 bot.polling(none_stop=True)
 
 connection.close()
-#kostik<33
+#kostik<3
+"""
+frekkrof"""
